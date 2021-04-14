b3567778612154efc0e035f0096612f416bd217d
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2021-04-14 03:15:55 -0700

    added error message

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const weather = require('./data/weather.json');
 const PORT = process.env.PORT || 3001;
-
+app.use(cors());
 app.get('/', (request, response) => {
   response.send('this is my first server! I became it');
 });
-app.get('/weather', (request, response) => {
-  // const forcastArr = weather.data.map(weatherObj => new Forecast(weatherObj.datetime, weatherObj.weather.description));
-  const forcastArr = weather.data.map(weatherObj => new Forecast(weatherObj));
-  // response.json(data);
-  response.send(forcastArr);
-});
+try {
+  app.get('/weather', (request, response) => {
+    // const forcastArr = weather.data.map(weatherObj => new Forecast(weatherObj.datetime, weatherObj.weather.description));
+    const forcastArr = weather.data.map(weatherObj => new Forecast(weatherObj));
+    // response.json(data);
+    response.send(forcastArr);
+  });
+} catch (error) {
+  // eslint-disable-next-line no-undef
+  handleErrors(error, response);
+}
 function Forecast(weatherObj) {
   this.date = weatherObj.datetime;
   this.description = `Low of ${weatherObj.low_temp}, high of ${weatherObj.max_temp} with ${weatherObj.weather.description}`;
-};
+}
+
+function handleErrors(error, response) {
+  response.status(500).send('internal error');
+}
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
